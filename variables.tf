53eac01b41b02cebc7915af60d2efdd0eea6c5b0
virender Rawat <virender.Rawat@outlook.com>
2025-12-04 14:11:54 +0530

    password and AdminID should enter from user

--- variables.tf
+++ variables.tf
@@ -4,3 +4,14 @@ variable "rgname" {
 variable "loc" {
   default = "eastus"
 }
+
+variable "userID" {
+  description = "Enter your user ID which going to cretae"
+  type        = string
+}
+
+variable "userPass" {
+  description = "Enter your password to login"
+  type        = string
+  sensitive   = true
+}
